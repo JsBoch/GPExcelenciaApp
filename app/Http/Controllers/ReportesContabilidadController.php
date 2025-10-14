e794a71b6ea5b4084ee6202a1ae41b25deca78bb
Jose Boch <jboch.99@gmail.com>
2025-10-14 14:41:21 -0600

    update reportes contabilidad

--- app/Http/Controllers/ReportesContabilidadController.php
+++ app/Http/Controllers/ReportesContabilidadController.php
@@ -244,7 +244,7 @@ class ReportesContabilidadController extends Controller
         }
 
         $q->where('cxc.saldo_pendiente', '>', 0);
-        $q->where("cxc.estado", ">", "1"); // Solo CxC activas
+        $q->where("cxc.estado", ">", "0"); // Solo CxC activas
 
         $rows = $q->orderBy('cli.nombre')
             ->orderBy('cxc.fecha_vencimiento')
