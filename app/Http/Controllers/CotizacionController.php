325a4de604b56ca116b9a4acf2a84af3feb729b4
Jose Boch <jboch.99@gmail.com>
2026-07-07 16:59:46 -0600

    unificacion de generación de pdf cotización

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -22,9 +22,17 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\QueryException;
 use App\Models\AdmEnvioItem;
 use App\Services\NotaEnvioService;
+use App\Services\CotizacionPdfService;
 
 class CotizacionController extends Controller
 {
+    private CotizacionPdfService $cotizacionPdfService;
+
+    public function __construct(
+        CotizacionPdfService $cotizacionPdfService
+    ) {
+        $this->cotizacionPdfService = $cotizacionPdfService;
+    }
     public function index(Request $request)
     {
         $user = Auth::user();
@@ -822,45 +830,52 @@ class CotizacionController extends Controller
             $cotizacionOriginal = AdmCotizacion::findOrFail($id);
             $fechaOriginal = $cotizacionOriginal->fecha_cotizacion;
 
-            $cotizacion = AdmCotizacion::where('c.idcotizacion', $id)
-                ->select(
-                    'c.idcotizacion',
-                    'c.nocotizacion',
-                    'c.fecha_cotizacion',
-                    't.tipo as tipo_pago',
-                    'c.total_general',
-                    'c.costear',
-                    'cl.nombre as cliente',
-                    'cl.nit as nit',
-                    'ct.nombre as contacto',
-                    'e.nombre as vendedor',
-                    'e.movil as telefono_vendedor',
-                    'e.correo_personal as correo_vendedor',
-                    'c.direccion_entrega',
-                    'c.observaciones_costeo',
-                    'c.observaciones_cliente',
-                    'c.costeo_observaciones',
-                    'c.trabajo',
-                    'c.version',
-                    'c.tipo_facturacion',
-                    'c.descuento_monto',
-                    'c.subtotal',
-                    'c.impuesto_iva',
-                    'c.total'
-                )
-                ->from('adm_cotizacion as c')
-                ->join('clientes as cl', 'c.idcliente', '=', 'cl.idcliente')
-                ->join('contacto_cliente as ct', 'c.idcontacto', '=', 'ct.id_contactocliente')
-                ->join('adm_empleados as e', 'c.idusuario', '=', 'e.iduser')
-                ->join('adm_tipo_pago as t', 'c.idtipopago', '=', 't.idtipopago')
-                ->first();
+            $cotizacion = $this->cotizacionPdfService->obtener($id);
 
             if (!$cotizacion) {
-                return response()->json(['message' => 'Cotización no encontrada'], 404);
+                return response()->json([
+                    'message' => 'Cotización no encontrada'
+                ], 404);
             }
+            // $cotizacion = AdmCotizacion::where('c.idcotizacion', $id)
+            //     ->select(
+            //         'c.idcotizacion',
+            //         'c.nocotizacion',
+            //         'c.fecha_cotizacion',
+            //         't.tipo as tipo_pago',
+            //         'c.total_general',
+            //         'c.costear',
+            //         'cl.nombre as cliente',
+            //         'cl.nit as nit',
+            //         'ct.nombre as contacto',
+            //         'e.nombre as vendedor',
+            //         'e.movil as telefono_vendedor',
+            //         'e.correo_personal as correo_vendedor',
+            //         'c.direccion_entrega',
+            //         'c.observaciones_costeo',
+            //         'c.observaciones_cliente',
+            //         'c.costeo_observaciones',
+            //         'c.trabajo',
+            //         'c.version',
+            //         'c.tipo_facturacion',
+            //         'c.descuento_monto',
+            //         'c.subtotal',
+            //         'c.impuesto_iva',
+            //         'c.total'
+            //     )
+            //     ->from('adm_cotizacion as c')
+            //     ->join('clientes as cl', 'c.idcliente', '=', 'cl.idcliente')
+            //     ->join('contacto_cliente as ct', 'c.idcontacto', '=', 'ct.id_contactocliente')
+            //     ->join('adm_empleados as e', 'c.idusuario', '=', 'e.iduser')
+            //     ->join('adm_tipo_pago as t', 'c.idtipopago', '=', 't.idtipopago')
+            //     ->first();
 
-            $detalles = AdmDetalleCotizacion::where('idcotizacion', $id)->get();
-            $cotizacion->detalles = $detalles;
+            // if (!$cotizacion) {
+            //     return response()->json(['message' => 'Cotización no encontrada'], 404);
+            // }
+
+            // $detalles = AdmDetalleCotizacion::where('idcotizacion', $id)->get();
+            // $cotizacion->detalles = $detalles;
             $cotizacion->fecha_cotizacion = $fecha_cot;
 
             $totalEnLetras = $this->convertirNumeroALetrasConCentavos($cotizacion->total);
@@ -912,6 +927,51 @@ class CotizacionController extends Controller
             DB::rollBack();
             return response()->json(['message' => 'Error al guardar el detalle de la cotización', 'error' => $e->getMessage()], 500);
         }
+    }
+
+    private function obtenerDatosPdfCotizacion($id)
+    {
+        $cotizacion = AdmCotizacion::where('c.idcotizacion', $id)
+            ->select(
+                'c.idcotizacion',
+                'c.nocotizacion',
+                'c.fecha_cotizacion',
+                't.tipo as tipo_pago',
+                'c.total_general',
+                'c.costear',
+                'cl.nombre as cliente',
+                'cl.nit as nit',
+                'ct.nombre as contacto',
+                'e.nombre as vendedor',
+                'e.movil as telefono_vendedor',
+                'e.correo_personal as correo_vendedor',
+                'c.direccion_entrega',
+                'c.observaciones_costeo',
+                'c.observaciones_cliente',
+                'c.costeo_observaciones',
+                'c.trabajo',
+                'c.version',
+                'c.tipo_facturacion',
+                'c.descuento_monto',
+                'c.subtotal',
+                'c.impuesto_iva',
+                'c.total',
+                'c.numero_interno'
+            )
+            ->from('adm_cotizacion as c')
+            ->join('clientes as cl', 'c.idcliente', '=', 'cl.idcliente')
+            ->join('contacto_cliente as ct', 'c.idcontacto', '=', 'ct.id_contactocliente')
+            ->join('adm_empleados as e', 'c.idusuario', '=', 'e.iduser')
+            ->join('adm_tipo_pago as t', 'c.idtipopago', '=', 't.idtipopago')
+            ->first();
+
+        if (!$cotizacion) {
+            return null;
+        }
+
+        $cotizacion->detalles = AdmDetalleCotizacion::where('idcotizacion', $id)->get();
+
+        return $cotizacion;
     }
 
     private function convertirNumeroALetrasConCentavos($numero)
